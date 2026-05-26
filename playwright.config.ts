fbd6f2d8c398784cced969e79fa0c17b033eb1e4
Jason Keung <work.jk4818@gmail.com>
2026-05-26 14:04:16 +0100

    FRH-46: updated pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 1,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Increase timeout for CI environments with slower startup and resource constraints. */
